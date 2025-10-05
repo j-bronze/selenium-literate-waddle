33229709dc8d1d6ebad6ef78e8946ccea338496f
yauheniya <evgeniya.boroznovskaya@gmail.com>
2025-10-05 08:53:48 +0300

    added selenium-test

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -10,6 +10,20 @@ import java.time.Duration;
 import java.util.List;
 
 public class SeleniumTest {
+    @Test
+    public void seleniumTestOpenBrowser() {
 
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
+
+        List<WebElement> listOfLinks = driver.findElements(By.cssSelector("a"));
+
+        Assert.assertEquals(listOfLinks.size(), 46);
+
+        listOfLinks.get(10).click();;
+
+    }
 
 }
