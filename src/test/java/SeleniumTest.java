b4e44de1c746e00b23cf5688a656e80f3de6ff99
yauheniya <evgeniya.boroznovskaya@gmail.com>
2025-10-05 08:35:56 +0300

    start the project

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class SeleniumTest {
+
+
+}
